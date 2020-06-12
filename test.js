d278e4b93b8c1c8bf5e43e14d4bb0b1e37ae7449
Kevin <kevinyluo@gmail.com>
2020-06-12 15:47:18 -0700

    test

--- test.js
+++ test.js
@@ -4,17 +4,16 @@ const cors = require('cors');
 
 const app = express();
 const port = 5000;
-const ip = 'localhost'
 
 // Configuring body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World, from express');
+app.get('/create-group', (req, res) => {
+  var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  res.send(ip);
 });
 
+app.set('trust proxy', true)
 app.listen(port, () => console.log(`Hello world app listening on port ${port}!`))
-
-module.exports = app;
-
