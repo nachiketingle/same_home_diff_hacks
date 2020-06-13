d707a4f667183125d1b48ec5eba70e698fea8815
Nachiket Ingle <nachiket.ingle@gmail.com>
2020-06-12 21:16:24 -0700

    Added basic map functionality

--- lib/Guest/GuestLobby.dart
+++ lib/Guest/GuestLobby.dart
@@ -1,31 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:samehomediffhacks/AppThemes.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
 
 class GuestLobby extends StatefulWidget {
   _GuestLobbyState createState() => _GuestLobbyState();
 }
 
 class _GuestLobbyState extends State<GuestLobby> {
+
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Guest Lobby Page"),
       ),
 
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text("This is the guest lobby page"),
-            RaisedButton(
-              color: AppThemes.primaryColor,
-              child: Text("To Categories", style: TextStyle(color: AppThemes.buttonTextColor),),
-              onPressed: () {
-                Navigator.pushNamed(context, "/categories");
-              },
-            )
-          ],
-        ),
+      body:GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
       ),
     );
   }
