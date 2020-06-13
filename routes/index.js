60447220db0c190fb3cb7aaee7a4817b84185f17
Kevin <kevinyluo@gmail.com>
2020-06-13 01:21:25 -0700

    create group works

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,44 @@
 var express = require('express');
 var router = express.Router();
-var mongo = require('../database/mongo');
+var mongo = require('../lib/mongo');
 const { v4: uuidv4 } = require('uuid');
 const ACCESS_LENGTH_CODE = 4;
 
 router.get('/get-restaurants', (req, res) => {
-  let list = [{"name": "In-N-Out",
-              "id": "WavvLdfdP6g8aZTtbBQHTw",
-              "rating": 4.5,
-              "review_count": 5296,
-              "price":"$",
-              "latitude":37.7670169511878, "longitude": -122.42184275, "photos": [
-              "https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg",
-              "https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg",
-              "https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg"]},
-              {"name": "mcdonalds",
-              "id": "WavvLdfdP6g8aZTtbBQHTw",
-              "rating": 1.2,
-              "review_count": 5453356,
-              "price":"$",
-              "latitude":31.7670169511878, "longitude": -121.42184275, "photos": [
-              "https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg",
-              "https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg",
-              "https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg"]},
-              {"name": "icecream",
-              "id": "WavvLdfdP6g8aZTtbBQHTw",
-              "rating": 5.3,
-              "review_count": 556,
-              "price":"$",
-              "latitude":31.7670169511878, "longitude": -121.42184275, "photos": [
-              "https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg",
-              "https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg",
-              "https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg"]}
+  let list = [{'name': 'In-N-Out',
+              'id': 'WavvLdfdP6g8aZTtbBQHTw',
+              'rating': 4.5,
+              'review_count': 5296,
+              'price':'$',
+              'latitude':37.7670169511878, 'longitude': -122.42184275, 'photos': [
+              'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
+              'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
+              'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg']},
+              {'name': 'mcdonalds',
+              'id': 'WavvLdfdP6g8aZTtbBQHTw',
+              'rating': 1.2,
+              'review_count': 5453356,
+              'price':'$',
+              'latitude':31.7670169511878, 'longitude': -121.42184275, 'photos': [
+              'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
+              'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
+              'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg']},
+              {'name': 'icecream',
+              'id': 'WavvLdfdP6g8aZTtbBQHTw',
+              'rating': 5.3,
+              'review_count': 556,
+              'price':'$',
+              'latitude':31.7670169511878, 'longitude': -121.42184275, 'photos': [
+              'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
+              'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
+              'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg']}
               ];
   res.json(list);
 })
 
-router.put('/create-group', (req, res) => {
+router.put('/create-group', async (req, res) => {
   // Generate an access code
-  let accessCode = (uuidv4().split("-"))[0].substring(0, ACCESS_LENGTH_CODE);
+  let accessCode = (uuidv4().split('-'))[0].substring(0, ACCESS_LENGTH_CODE);
 
   // Parse body
   let groupName = req.body['groupName'];
@@ -47,8 +47,8 @@ router.put('/create-group', (req, res) => {
   let latitude = req.body['latitude'];
   let longitude = req.body['longitude'];
 
-  let group = {};
-  group[accessCode] = {
+  let group = {
+    'accessCode': accessCode,
     'groupName': groupName,
     'maxDistance': maxDistance,
     'latitude': latitude,
@@ -58,26 +58,41 @@ router.put('/create-group', (req, res) => {
     'restuarant-pool': {}
   };
 
-  mongo.addDocument(group, 'group');
+
+  while(true){
+    let doc = await mongo.findDocument(accessCode, 'group');
+    console.log(doc);
+    if(doc == null){
+      mongo.addDocument(group, 'group');
+      break;
+    }
+    accessCode = (uuidv4().split('-'))[0].substring(0, ACCESS_LENGTH_CODE);
+    group['accessCode'] = accessCode;
+  }
 
   res.send(accessCode);
 });
 
-router.put('/join-group', (req, res) => {
+router.put('/join-group', async (req, res) => {
   // Parse body
   let accessCode = req.body['accessCode'];
   let name = req.body['name'];
-  console.log("ACCESS CODE: " + accessCode);
 
-  mongo.findDocument(accessCode, "group");
+  const doc = await mongo.findDocument(accessCode, 'group');
+  if (doc['members'].includes(name)){
+    res.sendStatus(409);
+    return;
+  }
+  doc['members'].push(name);
 
+  mongo.updateDocument(accessCode, 'members', doc['members'], 'group');
   res.sendStatus(200);
+
 });
 
 router.put('/start-category', (req, res) => {
   // Parse body
   let accessCode = req.body['accessCode'];
-
   res.sendStatus(200);
 });
 
