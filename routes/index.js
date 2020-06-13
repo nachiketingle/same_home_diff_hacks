7d72149c1a9a4543bdcafa69e8cede570ea9301f
Aaron Chen Laptop <chenaaron3@gmail.com>
2020-06-13 02:35:51 -0700

    fixed .env

--- routes/index.js
+++ routes/index.js
@@ -4,55 +4,63 @@ var mongo = require('../lib/mongo');
 const pusher = require('../lib/pusher');
 const { v4: uuidv4 } = require('uuid');
 const ACCESS_LENGTH_CODE = 4;
-const CATEGORIES = {'tradamerican': 'American',
-                    'asianfusion': 'Asian Fushion',
-                    'bbq': 'Barbeque',
-                    'breakfast_brunch': 'Breakfast & Brunch',
-                    'buffets': 'Buffets',
-                    'burgers': 'Burgers',
-                    'cafes': 'Cafes',
-                    'chicken_wings': 'Chicken Wings',
-                    'chinese': 'Chinese',
-                    'comfortfood': 'Comfort Food',
-                    'korean': 'Korean',
-                    'japanese': 'Japanese',
-                    'thai': 'Thai',
-                    'vegetarian': 'Vegetarian',
-                    'vietnamese': 'Vietnamese'};
+const CATEGORIES = {
+  'tradamerican': 'American',
+  'asianfusion': 'Asian Fushion',
+  'bbq': 'Barbeque',
+  'breakfast_brunch': 'Breakfast & Brunch',
+  'buffets': 'Buffets',
+  'burgers': 'Burgers',
+  'cafes': 'Cafes',
+  'chicken_wings': 'Chicken Wings',
+  'chinese': 'Chinese',
+  'comfortfood': 'Comfort Food',
+  'korean': 'Korean',
+  'japanese': 'Japanese',
+  'thai': 'Thai',
+  'vegetarian': 'Vegetarian',
+  'vietnamese': 'Vietnamese'
+};
 
 router.get('/', (req, res) => {
   res.send("Hi Victor.")
 })
 
 router.get('/get-restaurants', (req, res) => {
-  let list = [{'name': 'In-N-Out',
-              'id': 'WavvLdfdP6g8aZTtbBQHTw',
-              'rating': 4.5,
-              'review_count': 5296,
-              'price':'$',
-              'latitude':37.7670169511878, 'longitude': -122.42184275, 'photos': [
-              'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
-              'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
-              'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg']},
-              {'name': 'mcdonalds',
-              'id': 'WavvLdfdP6g8aZTtbBQHTw',
-              'rating': 1.2,
-              'review_count': 5453356,
-              'price':'$',
-              'latitude':31.7670169511878, 'longitude': -121.42184275, 'photos': [
-              'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
-              'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
-              'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg']},
-              {'name': 'icecream',
-              'id': 'WavvLdfdP6g8aZTtbBQHTw',
-              'rating': 5.3,
-              'review_count': 556,
-              'price':'$',
-              'latitude':31.7670169511878, 'longitude': -121.42184275, 'photos': [
-              'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
-              'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
-              'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg']}
-              ];
+  let list = [{
+    'name': 'In-N-Out',
+    'id': 'WavvLdfdP6g8aZTtbBQHTw',
+    'rating': 4.5,
+    'review_count': 5296,
+    'price': '$',
+    'latitude': 37.7670169511878, 'longitude': -122.42184275, 'photos': [
+      'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
+      'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
+      'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg']
+  },
+  {
+    'name': 'mcdonalds',
+    'id': 'WavvLdfdP6g8aZTtbBQHTw',
+    'rating': 1.2,
+    'review_count': 5453356,
+    'price': '$',
+    'latitude': 31.7670169511878, 'longitude': -121.42184275, 'photos': [
+      'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
+      'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
+      'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg']
+  },
+  {
+    'name': 'icecream',
+    'id': 'WavvLdfdP6g8aZTtbBQHTw',
+    'rating': 5.3,
+    'review_count': 556,
+    'price': '$',
+    'latitude': 31.7670169511878, 'longitude': -121.42184275, 'photos': [
+      'https://s3-media2.fl.yelpcdn.com/bphoto/CPc91bGzKBe95aM5edjhhQ/o.jpg',
+      'https://s3-media4.fl.yelpcdn.com/bphoto/FmXn6cYO1Mm03UNO5cbOqw/o.jpg',
+      'https://s3-media4.fl.yelpcdn.com/bphoto/HZVDyYaghwPl2kVbvHuHjA/o.jpg']
+  }
+  ];
   res.json(list);
 })
 
@@ -78,11 +86,10 @@ router.put('/create-group', async (req, res) => {
     'restuarant-pool': {}
   };
 
-
-  while(true){
+  while (true) {
     let doc = await mongo.findDocument(accessCode, 'group');
     console.log(doc);
-    if(doc == null){
+    if (doc == null) {
       mongo.addDocument(group, 'group');
       break;
     }
@@ -99,7 +106,7 @@ router.put('/join-group', async (req, res) => {
   let name = req.body['name'];
 
   const doc = await mongo.findDocument(accessCode, 'group');
-  if (doc['members'].includes(name)){
+  if (doc['members'].includes(name)) {
     res.sendStatus(409);
     return;
   }
@@ -108,15 +115,14 @@ router.put('/join-group', async (req, res) => {
   mongo.updateDocument(accessCode, 'members', doc['members'], 'group');
 
   // Send pusher triggerEvent
-  //pusher.triggerEvent(accessCode, 'onGuestJoin', doc['members']);
+  pusher.triggerEvent(accessCode, 'onGuestJoin', JSON.stringify(doc['members']));
   res.sendStatus(200);
-
 });
 
 router.put('/start-category', (req, res) => {
   // Parse body
   let accessCode = req.body['accessCode'];
-  //pusher.triggerEvent(accessCode, 'onCategoryStart', JSON.stringify(CATEGORIES));
+  pusher.triggerEvent(accessCode, 'onCategoryStart', JSON.stringify(CATEGORIES));
   res.json(CATEGORIES);
 });
 
