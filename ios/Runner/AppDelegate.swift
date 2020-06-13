9130f2ba7ca09adb8d0f84c404d105f559d19511
Victor Zhu <victorvctr.zh@gmail.com>
2020-06-13 01:42:42 -0700

    Updated files for IOS simulator build

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -9,7 +9,7 @@ import Foundation
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey(ProcessInfo.processInfo.environment["MAPS_API_KEY_IOS"])
+    GMSServices.provideAPIKey("yes")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
