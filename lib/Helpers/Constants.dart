5f43a643c98dca90294896b595067c1e0e617db8
Nachiket Ingle <nachiket.ingle@gmail.com>
2020-06-14 02:58:15 -0700

    Bug fixes again!

--- lib/Helpers/Constants.dart
+++ lib/Helpers/Constants.dart
@@ -5,5 +5,5 @@ class Constants {
   static String startCategories = "start-category";
   static String setCategories = "set-categories";
   static String submitSwipes = "submit-swipes";
-  static String accessCode = "62e5";
+
 }
